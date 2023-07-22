0bc04ee1f9df8a8598bed47a5f269e6b0542a0e2
Sean Park <seanpark132@gmail.com>
2023-07-22 07:40:35 -0700

    removed redux npm packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { configureStore } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-const store = configureStore({
-	reducer: {},
-});
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<QueryClientProvider client={queryClient}>
-				<App />
-				<ReactQueryDevtools />
-			</QueryClientProvider>
-		</Provider>
+		<QueryClientProvider client={queryClient}>
+			<App />
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	</React.StrictMode>
 );
