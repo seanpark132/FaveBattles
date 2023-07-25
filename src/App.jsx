6383683e8e9a8938ba7d209a397140ce589a0ed5
Sean Park <seanpark132@gmail.com>
2023-07-25 12:46:52 -0700

    browserRouter, navbar from App to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Game from "./pages/Game";
 import Rankings from "./pages/Rankings";
@@ -11,7 +11,6 @@ import Profile from "./pages/Profile";
 import ResetPassword from "./pages/ResetPassword";
 import EditGame from "./pages/EditGame";
 import NoPage from "./pages/NoPage";
-import Navbar from "./components/Navbar";
 import "./css/Profile.css";
 import "./css/SignUp.css";
 import "./css/App.css";
@@ -27,64 +26,58 @@ export default function App() {
 		queryFn: () => getAllGameData(),
 	});
 
-	if (allGameDataQuery.isLoading) return <h1>Loading...</h1>;
+	if (allGameDataQuery.isLoading) return <h1 className="m-6">Loading...</h1>;
 	if (allGameDataQuery.isError) {
-		return <h1>An error occurred. Please try refreshing the page.</h1>;
+		return (
+			<h1 className="m-6">
+				An error occurred. Please try refreshing the page.
+			</h1>
+		);
 	}
 
 	return (
 		<>
-			<BrowserRouter>
-				<Navbar />
-				<Routes>
+			<Routes>
+				<Route
+					index
+					element={
+						<Home
+							allGamesData={allGameDataQuery.data.allGamesData}
+						/>
+					}
+				/>
+				<Route path="/create" element={<Create />} />
+				<Route path="/create-img" element={<CreateImg />} />
+				<Route path="/create-video" element={<CreateVideo />} />
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/reset-password" element={<ResetPassword />} />
+				<Route path="/profile" element={<Profile />} />
+				{allGameDataQuery.data.allGameIds.map((id) => (
 					<Route
-						index
+						key={id}
+						path={`/edit-game/${id}`}
+						element={<EditGame key={id} gameId={id} />}
+					/>
+				))}
+				{allGameDataQuery.data.allGamesData.map((gameData) => (
+					<Route
+						key={gameData.id}
+						path={`/game/${gameData.id}`}
+						element={<Game key={gameData.id} gameData={gameData} />}
+					/>
+				))}
+				{allGameDataQuery.data.allGamesData.map((gameData) => (
+					<Route
+						key={gameData.id}
+						path={`/stats/${gameData.id}`}
 						element={
-							<Home
-								allGamesData={
-									allGameDataQuery.data.allGamesData
-								}
-							/>
+							<Rankings key={gameData.id} gameData={gameData} />
 						}
 					/>
-					<Route path="/create" element={<Create />} />
-					<Route path="/create-img" element={<CreateImg />} />
-					<Route path="/create-video" element={<CreateVideo />} />
-					<Route path="/sign-up" element={<SignUp />} />
-					<Route path="/sign-in" element={<SignIn />} />
-					<Route path="/reset-password" element={<ResetPassword />} />
-					<Route path="/profile" element={<Profile />} />
-					{allGameDataQuery.data.allGameIds.map((id) => (
-						<Route
-							key={id}
-							path={`/edit-game/${id}`}
-							element={<EditGame key={id} gameId={id} />}
-						/>
-					))}
-					{allGameDataQuery.data.allGamesData.map((gameData) => (
-						<Route
-							key={gameData.id}
-							path={`/game/${gameData.id}`}
-							element={
-								<Game key={gameData.id} gameData={gameData} />
-							}
-						/>
-					))}
-					{allGameDataQuery.data.allGamesData.map((gameData) => (
-						<Route
-							key={gameData.id}
-							path={`/stats/${gameData.id}`}
-							element={
-								<Rankings
-									key={gameData.id}
-									gameData={gameData}
-								/>
-							}
-						/>
-					))}
-					<Route path="*" element={<NoPage />} />
-				</Routes>
-			</BrowserRouter>
+				))}
+				<Route path="*" element={<NoPage />} />
+			</Routes>
 		</>
 	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Navbar from "./components/Navbar";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthContextProvider } from "./context/AuthContext";
@@ -17,8 +19,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<AuthContextProvider>
-				<App />
-				<ReactQueryDevtools />
+				<BrowserRouter>
+					<Navbar />
+					<App />
+					<ReactQueryDevtools />
+				</BrowserRouter>
 			</AuthContextProvider>
 		</QueryClientProvider>
 	</React.StrictMode>
