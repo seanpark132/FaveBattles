e21c4a2cee6fdef611c754961b58eb8310dbba3d
Sean Park <seanpark132@gmail.com>
2023-07-25 12:39:23 -0700

    react query: disabled refetch on focus

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthContextProvider } from "./context/AuthContext";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
