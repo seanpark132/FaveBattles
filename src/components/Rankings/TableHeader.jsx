0fd2584b3541e6391d0a0b247033624def97c0ce
Sean Park <seanpark132@gmail.com>
2024-03-15 16:17:22 -0700

    small style change to header

--- src/components/Rankings/TableHeader.jsx
+++ src/components/Rankings/TableHeader.jsx
@@ -18,7 +18,9 @@ export const TableHeader = ({ gameData, theme, setFilters }) => {
           className={`py-1 pl-9 pr-2  ${theme === "dark" ? "bg-slate-700" : ""}`}
         />
       </span>
-      <h2 className="mt-4 text-center text-lg lg:text-2xl">
+      <h2
+        className={`mt-4 text-center text-lg lg:text-2xl ${theme === "dark" ? "text-white" : ""} `}
+      >
         [{gameData.mainCategory}] {gameData.title}
       </h2>
     </div>
