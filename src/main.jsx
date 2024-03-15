9b7bfae9c7375bc3cc828f42865244e84f64edfe
Sean Park <seanpark132@gmail.com>
2024-03-15 11:56:02 -0700

    updated primereact version, table style fixes

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ThemeContextProvider } from "./context/ThemeContext";
+import { PrimeReactProvider } from "primereact/api";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,13 +18,15 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <AuthContextProvider>
-      <ThemeContextProvider>
-        <BrowserRouter>
-          <App />
-          <ReactQueryDevtools />
-        </BrowserRouter>
-      </ThemeContextProvider>
-    </AuthContextProvider>
+    <PrimeReactProvider value={{}}>
+      <AuthContextProvider>
+        <ThemeContextProvider>
+          <BrowserRouter>
+            <App />
+            <ReactQueryDevtools />
+          </BrowserRouter>
+        </ThemeContextProvider>
+      </AuthContextProvider>
+    </PrimeReactProvider>
   </QueryClientProvider>,
 );
